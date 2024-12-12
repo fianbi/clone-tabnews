e0f8bdd5bd5ad9f3a916fd5682618b7a0596dd90
Filipe Bittencourt <filipe.bitt10@gmail.com>
2024-12-12 19:49:13 +0000

    a frase final

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Modifica a frase</h1>
+    return <h1>"No temor ao Senhor, o homem encontra um forte apoio e também segurança para a sua família."<br></br>Provérbios 14:26 NTLH</h1>
 }
 
 export default Home;
